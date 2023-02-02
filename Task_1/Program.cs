d9d01043643b54e1b0eeaaa8421d9ea9bbe68913
Anna <anyutka901990@mail.ru>
2023-02-02 23:19:44 +0300

    Task1

---
+++ Task_1/Program.cs
@@ -0,0 +1,9 @@
+﻿// Составить алгоритм: если введенное число больше 7, то вывести “Привет”
+
+Console.WriteLine("Введите число: ");
+int number=Convert.ToInt32(Console.ReadLine());
+if (number>7)
+{
+    Console.WriteLine("Привет");
+}
+
