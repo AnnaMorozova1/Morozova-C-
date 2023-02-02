8fe9cc724e118bdf60748653217248263c552dfe
Anna <anyutka901990@mail.ru>
2023-02-02 23:21:36 +0300

    Task3

---
+++ Task_3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Составить алгоритм: на входе есть числовой массив, 
+// необходимо вывести элементы массива кратные 3
+
+Console.WriteLine("Ввести колличество элементов: ");
+int Count = Convert.ToInt32(Console.ReadLine());
+int[] mass = new int[Count];
+Console.WriteLine("Ввести элементы массива: ");
+for (int i=0; i<mass.Length; i++)
+{
+    mass[i]=Convert.ToInt32(Console.ReadLine());  
+ }
+Console.WriteLine("Числа кратные 3: ");
+for (int i=0; i<mass.Length; i++)
+{
+    if (mass[i] % 3 == 0) 
+    {
+        Console.WriteLine(mass[i]+" "); 
+    }
+}
